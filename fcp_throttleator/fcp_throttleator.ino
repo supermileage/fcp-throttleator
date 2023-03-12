c8caf2ddb6a004b9ec1c6d106d41ed2400b42bc6
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2023-03-11 16:45:53 -0800

    small motor fix scaling

--- fcp_throttleator/fcp_throttleator.ino
+++ fcp_throttleator/fcp_throttleator.ino
@@ -1,6 +1,6 @@
 #define POT_MIN 370
 #define POT_MAX 650
-#define MAX_GRAD 10
+#define MAX_GRAD 1000
 
 uint32_t normalized_in;
 uint32_t throttle = 0;
@@ -8,6 +8,7 @@ uint16_t normalized_throttle = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  TCCR0B = TCCR0B & B11111000 | B00000010; // for PWM frequency of 7812.50 Hz
   Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(5, OUTPUT);
@@ -18,6 +19,8 @@ void loop() {
   uint32_t pot_in = analogRead(A0);
   if (pot_in < POT_MIN)  pot_in = POT_MIN;
   else if (pot_in > POT_MAX) pot_in = POT_MAX;
+
+  // Input from 0 - 1023
   normalized_in = (pot_in - POT_MIN) * 1023 / (POT_MAX - POT_MIN);
 
   if (normalized_in < throttle) {
@@ -26,7 +29,7 @@ void loop() {
     throttle = (normalized_in - throttle) > MAX_GRAD ? throttle += MAX_GRAD : normalized_in;
   }
 
-  normalized_throttle = throttle * 255 / 1023 / 1.5;
+  normalized_throttle = throttle;
 
   analogWrite(5, normalized_throttle);
 
